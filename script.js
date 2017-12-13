8cb62b5b462cdf8d458f79af70ac8ed98f9278a5
Mike Nabil <maikel_nabil@yahoo.com>
2017-12-13 16:39:56 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(() => {
+  $('.js-get').on('click', e => {
+    e.preventDefault()
+    console.log('clicked!')
+  })
+
+  $('.js-post').on('click', e => {
+    e.preventDefault()
+    console.log('clicked!')
+  })
+
+  $('.js-put').on('click', e => {
+    e.preventDefault()
+    console.log('clicked!')
+  })
+
+  $('.js-delete').on('click', e => {
+    e.preventDefault()
+    console.log('clicked!')
+  })
+})
