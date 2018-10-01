88014f2f45a80ccd70acb519116cac386aded229
Ryan OKeefe <ryanokeefe208@gmail.com>
2018-10-01 17:23:54 -0400

    backup

--- script.js
+++ script.js
@@ -1,21 +1,56 @@
 $(document).ready(() => {
   $('.js-get').on('click', e => {
     e.preventDefault()
-    console.log('clicked!')
+    $.ajax({
+      url: 'https://ga-tunr-ajax.herokuapp.com/artists',
+      type: 'GET',
+      dataType: 'json'
+    }).done((response) => {
+      console.log(response)
+    })
   })
 
   $('.js-post').on('click', e => {
     e.preventDefault()
-    console.log('clicked!')
+  $.ajax({
+    url: 'https://ga-tunr-ajax.herokuapp.com/artists',
+    type: 'POST',
+    dataType: 'json',
+    data: {
+      artist: {
+        name: 'Run the Jewels',
+        nationality: 'USA',
+    }}
+    }).done((response) => {
+      console.log(response)
+    })  
   })
 
   $('.js-put').on('click', e => {
     e.preventDefault()
-    console.log('clicked!')
+    $.ajax({
+      type: 'PUT',
+      datatype: 'json',
+      url: 'https://ga-tunr-ajax.herokuapp.com/artists/60',
+      data: {
+        artist: {
+          photo_url: 'https://www.istockphoto.com/resources/images/PhotoFTLP/img_67920257.jpg'
+        }
+      }      
+
+    }).done((response) => {
+      console.log(response)
+    })
   })
 
   $('.js-delete').on('click', e => {
     e.preventDefault()
-    console.log('clicked!')
+    $.ajax({
+      type: 'DELETE',
+      dataType: 'json',
+      url: 'https://ga-tunr-ajax.herokuapp.com/artists/23'
+    }).done((response) => {
+      console.log('Deleted')
+    })
   })
 })
